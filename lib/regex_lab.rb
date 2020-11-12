c046dbfad4632b4398f61ed03dca47f1d7252d3f
Jess Pesale <jess_pesale@aol.com>
2020-11-12 03:14:52 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,6 @@
 def starts_with_a_vowel?(word)
-  if word.match(/\A+[aeiou]/) == true
-    return true
-  else
-    return false  
-  end
+  return word.match(/\A+[aeiou]/).length > 0
+  
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
